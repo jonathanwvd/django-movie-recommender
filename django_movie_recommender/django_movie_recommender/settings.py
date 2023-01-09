35fd7c5fe2cbe66e124702d695780fee73df6f5e
Jônathan Dambros <jonathan@latos.ai>
2023-01-09 09:57:52 -0300

    add static folder to heroku

--- django_movie_recommender/django_movie_recommender/settings.py
+++ django_movie_recommender/django_movie_recommender/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "django_movie_recommender/static"),
+
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
