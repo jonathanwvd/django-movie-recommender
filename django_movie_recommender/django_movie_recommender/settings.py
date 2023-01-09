24bf35a9c0af60fa1fa9f4d08b5743075fc5ede9
Jônathan Dambros <jonathan@latos.ai>
2023-01-09 10:00:55 -0300

    fix heroku static files

--- django_movie_recommender/django_movie_recommender/settings.py
+++ django_movie_recommender/django_movie_recommender/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,8 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "django_movie_recommender/static"),
-
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
